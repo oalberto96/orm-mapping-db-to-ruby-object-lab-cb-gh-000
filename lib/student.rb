490ed2344a537d8b42f6de6e06c247390f098622
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-13 03:09:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,10 +30,12 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
     SELECT * FROM students
-
+    WHERE students.grade < 12
     SQL
     result = DB[:conn].execute(sql)
-    binding.pry
+    students = []
+    result.each {|row| students << self.new_from_db(row) }
+    students
   end
 
   def self.find_by_name(name)
