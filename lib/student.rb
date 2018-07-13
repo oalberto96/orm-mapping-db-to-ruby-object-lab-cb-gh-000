7fd74ebfb9c708ac22a08134b4911e7c3253b70f
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-13 03:15:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    self.first_X_students_in_grade_10(1).first
   end
 
   def self.array_from_raw_data(data)
